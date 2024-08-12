2f71adadb3be31a470ee6a5f140fd3989ea2d6bf
AbdulRehman4481 <abdulrehmanmughal4481@gmail.com>
2024-08-12 18:52:29 +0100

    feat:Layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className}`}>
+      <body className={inter.className}>
         <NextUIProvider>{children}</NextUIProvider>
       </body>
     </html>
